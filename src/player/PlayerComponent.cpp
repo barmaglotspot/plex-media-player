8d51cec8e25a8c8b4cf6160aeb8de6f03c62e746
LongChair <longchair@hotmail.com>
2016-10-07 07:50:46 +0200

    AML : add proper hwdec option to mpv
    this allows to use the right ffmpeg hardware decoder for amlogic

--- src/player/PlayerComponent.cpp
+++ src/player/PlayerComponent.cpp
@@ -128,6 +128,11 @@ bool PlayerComponent::componentInitialize()
   mpv::qt::set_option_variant(m_mpv, "hr-seek", "no");
 #endif
 
+#ifdef TARGET_AML
+  // AML needs its own hardware decoder
+  mpv::qt::set_option_variant(m_mpv, "hwdec", "aml");
+#endif
+
   mpv_observe_property(m_mpv, 0, "pause", MPV_FORMAT_FLAG);
   mpv_observe_property(m_mpv, 0, "core-idle", MPV_FORMAT_FLAG);
   mpv_observe_property(m_mpv, 0, "cache-buffering-state", MPV_FORMAT_INT64);
